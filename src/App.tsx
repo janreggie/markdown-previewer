84c5ad72038575afa50c00ad299a16bd6caf27d2
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2020-09-16 16:32:03 +0800

    Initialized React application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App" id="app">
+      There's nothing here yet.
+    </div>
+  );
+}
+
+export default App;
